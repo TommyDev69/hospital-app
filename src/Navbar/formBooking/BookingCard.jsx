57bc13677ce997cad1c03d0e522304db9caf624a
TommyDev69 <sunnevian4life@gmail.com>
2024-11-29 03:27:36 +0100

    linux

--- src/Navbar/formBooking/BookingCard.jsx
+++ src/Navbar/formBooking/BookingCard.jsx
@@ -6,7 +6,7 @@ const BookingCard = () => {
     return (  
     
 
-        <div className="relative min-w-full md:pl-[21px] lg:pl-[58px] pl-[12px]  md:min-h-[350px] min-h-[478px] container-fluid h-[300px] pb-[40px]">
+        <div className="relative min-w-full md:pl-[21px] lg:pl-[58px] pl-[32px]  md:min-h-[350px] min-h-[478px] container-fluid h-[300px] pb-[40px]">
 
             <div className="shadow-2xl rounded-xl overflow-x-hidden absolute bg-red-800 top-[-190px] md:w-[90%] shadow-[#0cb7d6] w-[80%]  my-10 ">
                 <Receipt />

7e5a8bee338f24b35927d63f630be28df521ad1c
TommyDev69 <sunnevian4life@gmail.com>
2024-11-29 03:37:32 +0100

    hospital a;pp

--- src/Navbar/formBooking/BookingCard.jsx
+++ src/Navbar/formBooking/BookingCard.jsx
@@ -8,7 +8,7 @@ const BookingCard = () => {
 
         <div className="relative min-w-full md:pl-[40px] lg:pl-[58px] pl-[32px]  md:min-h-[350px] min-h-[478px] container-fluid h-[300px] pb-[40px]">
 
-            <div className="shadow-2xl rounded-xl overflow-x-hidden absolute bg-red-800 top-[-190px] md:w-[90%] shadow-[#0cb7d6] w-[80%]  my-10 ">
+            <div className="shadow-2xl rounded-xl overflow-x-hidden absolute bg-white top-[-190px] md:w-[90%] shadow-[#0cb7d6] w-[80%]  my-10 ">
                 <Receipt />
                 {/* <FormOperations /> */}
                 
